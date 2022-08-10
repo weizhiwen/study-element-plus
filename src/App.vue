d2a72c2f6f3eeace1150181e06a71693583d5539
shixin <weizhiwen23@gmail.com>
2022-08-10 23:10:57 +0800

    初始化程序

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { useDark, useToggle } from '@vueuse/core'
+const isDark = useDark()
+const toggleDark = useToggle(isDark)
+</script>
+
+<template>
+  <ElButton @click="toggleDark()">切换暗黑模式</ElButton>
+  <ElButton>测试</ElButton>
+  <ElButton type="primary">测试</ElButton>
+  <p class="text-amber-500">1111</p>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import './style.css'
+import './styles/tailwind.css'
+import App from './App.vue'
+import 'element-plus/theme-chalk/dark/css-vars.css'
+
+createApp(App).mount('#app')
